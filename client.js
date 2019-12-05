948c83c58f27e3e082c12d6e7dc96d06a4f0255e
oceanway1 <oceanway1@yahoo.ca>
2019-12-05 01:02:24 +0000

    3 changed

--- client.js
+++ client.js
@@ -3,31 +3,59 @@ const net = require('net');
 /**
  * Establishes connection with the game server
  */
-const connect = function () {
+const connect = function() {
   const conn = net.createConnection({
     host: '192.168.88.149',
     port: 50541
   });
-  conn.setEncoding('utf8');
-  conn.on('connect', () => {
-
-    conn.write("successfully connect on the server");
-    conn.write('Name: LN');
-
-  });
-    conn.on('connect', () => {
-      setTimeout(function() {
-    conn.write("Move: up"); }, 2000);
-
-    // conn.write("Move: down");
-    // conn.write("Move: left");
-    // conn.write("Move: right");
-
-  });
   // interpret incoming data as text
+  conn.setEncoding('utf8');
 
+  conn.on('connect', () => {
+    conn.write('Successfully connected to game server');
+    conn.write('Name: MKL');
+  });
+
+  // conn.on('connect', () => {
+  //   setInterval(function() {
+  //     conn.write("Move: up");
+  //   }, 50);
+  // });
 
   return conn;
 }
 
 module.exports = connect;
+// const net = require('net');
+
+// /**
+//  * Establishes connection with the game server
+//  */
+// const connect = function () {
+//   const conn = net.createConnection({
+//     host: '192.168.88.149',
+//     port: 50541
+//   });
+//   conn.setEncoding('utf8');
+//   conn.on('connect', () => {
+
+//     conn.write("successfully connect on the server");
+//     conn.write('Name: LN');
+
+//   });
+//     conn.on('connect', () => {
+//       setTimeout(function() {
+//     conn.write("Move: up"); }, 2000);
+
+//     // conn.write("Move: down");
+//     // conn.write("Move: left");
+//     // conn.write("Move: right");
+
+//   });
+//   // interpret incoming data as text
+
+
+//   return conn;
+// }
+
+// module.exports = connect;
--- play.js
+++ play.js
@@ -1,47 +1,54 @@
 const connect = require('./client');
+const setupInput = require('./input');
+
 console.log('Connecting ...');
 connect();
-// const net = require('net');
-const handleUserInput = function (key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
-  // if (key && key.ctrl && key.name == 'c') process.exit();
-  // process.exit();
-}
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
 
-  stdin.on('data', handleUserInput)
-
-  // setTimeout(() => {
-  //   process.exit();
-  // },5000)
-
-  return stdin;
-}
 setupInput();
-
-// /**
-//  * Establishes connection with the game server
-//  */
-// const connect = function() {
-//   const conn = net.createConnection({ 
-//     host: '192.168.88.149',
-//     port: 50541
-//   });
-//   // interpret incoming data as text
-//   conn.setEncoding('utf8'); 
-//   conn.on('connect', () => {
-//     conn.write("successfully connect on the server");
-//     conn.write('Name:LN');
-//   });
-//   return conn;
-// }
-
+// const connect = require('./client');
 // console.log('Connecting ...');
 // connect();
+// // const net = require('net');
+// const handleUserInput = function (key) {
+//   if (key === '\u0003') {
+//     process.exit();
+//   }
+//   // if (key && key.ctrl && key.name == 'c') process.exit();
+//   // process.exit();
+// }
+// const setupInput = function() {
+//   const stdin = process.stdin;
+//   stdin.setRawMode(true);
+//   stdin.setEncoding('utf8');
+//   stdin.resume();
+
+//   stdin.on('data', handleUserInput)
+
+//   // setTimeout(() => {
+//   //   process.exit();
+//   // },5000)
+
+//   return stdin;
+// }
+// setupInput();
+
+// // /**
+// //  * Establishes connection with the game server
+// //  */
+// // const connect = function() {
+// //   const conn = net.createConnection({ 
+// //     host: '192.168.88.149',
+// //     port: 50541
+// //   });
+// //   // interpret incoming data as text
+// //   conn.setEncoding('utf8'); 
+// //   conn.on('connect', () => {
+// //     conn.write("successfully connect on the server");
+// //     conn.write('Name:LN');
+// //   });
+// //   return conn;
+// // }
+
+// // console.log('Connecting ...');
+// // connect();
 

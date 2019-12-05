9dcb9f3ed4d72c51d08eaab482dd9ca60aab5f30
oceanway1 <oceanway1@yahoo.ca>
2019-12-05 05:56:37 +0000

    final modification

--- input.js
+++ input.js
@@ -1,19 +1,42 @@
+let connection;
+
 const handleUserInput = function(key) {
   if (key === "\u0003") {
     process.exit();
   }
+  if (key === "w") {
+    connection.write('Move: up');
+  }
+  if (key === "a") {
+    connection.write('Move: left');
+  }
+  if (key === "s") {
+    connection.write('Move: down');
+  }
+  if (key === "d") {
+    connection.write('Move: right');
+  }
+  if (key === "h") {
+    connection.write('Say: Hey');
+  }
+  if (key === "i") {
+    connection.write('Say: Ho');
+  }
+  if (key === "o") {
+    connection.write('Say: oh yeah!');
+  }
 };
 
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
 
   stdin.on('data', handleUserInput)
+
   return stdin;
 };
-
-setupInput();
 
 module.exports = setupInput;
